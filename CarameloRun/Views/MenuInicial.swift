7c196525037f19520f15e42f5369e4bd4a4b8f29
caio melloni <caiomelloni.contato@gmail.com>
2023-10-19 11:14:28 -0300

    add menu inicial vc

---
+++ CarameloRun/Views/MenuInicial.swift
@@ -0,0 +1,43 @@
+//
+//  MenuInicial.swift
+//  CarameloRun
+//
+//  Created by Caio Melloni dos Santos on 19/10/23.
+//
+
+import Foundation
+import UIKit
+
+class MenuInicialViewController: UIViewController {
+    let stackView = UIStackView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension MenuInicialViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Welcome"
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+}
