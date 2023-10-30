56b0a022e393b55b493b18b35f83f2415222a007
caio melloni <caiomelloni.contato@gmail.com>
2023-10-30 08:20:31 -0300

    comeca adicao da maquina de estado

---
+++ CarameloRun/Gameplay/States/Player/RunningState.swift
@@ -0,0 +1,28 @@
+//
+//  RunningState.swift
+//  CarameloRun
+//
+//  Created by Caio Melloni dos Santos on 29/10/23.
+//
+
+import GameplayKit
+
+class RunningState: GKState {
+    override func didEnter(from previousState: GKState?) {
+        // runs as it enters this state
+        // has access to the previous state
+    }
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+        // returns true if can go to the next state
+        return true
+    }
+    
+    override func willExit(to nextState: GKState) {
+        // performs action when exiting this state
+    }
+    
+    override func update(deltaTime seconds: TimeInterval) {
+        
+    }
+}
