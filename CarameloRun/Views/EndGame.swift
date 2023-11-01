a79219be1c16a6521ace590a6e9d6bd1d1139090
Luis Paulo Siqueira <107970831+luispsiqueira@users.noreply.github.com>
2023-11-01 09:21:16 -0300

    adicionando tela de resultados

--- CarameloRun/Views/EndGame.swift
+++ CarameloRun/Views/EndGame.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 import UIKit
+import GameKit
 
 
-class EndGame: UIViewController {
+class EndGame: UIViewController, GKGameCenterControllerDelegate {
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        print("eita")
+    }
+    
     override func viewDidLoad() {
             super.viewDidLoad()
 
@@ -30,8 +35,18 @@ class EndGame: UIViewController {
             // Defina restrições para centralizar a label na tela
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
             label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        }
+        
+        
+        //tentando colocar a leaderboard do próprio gameCenter
+        let vc = GKGameCenterViewController(state: .leaderboards)
+        vc.gameCenterDelegate = self
+        present(vc, animated: true, completion: nil)
+        
+    }
+    
 }
 
 
-
+class LeaderBoard: GKLeaderboard {
+    
+}
