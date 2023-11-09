ed60a38c52b4656cda2667d85de5122dd399ab33
caio melloni <caiomelloni.contato@gmail.com>
2023-11-09 11:47:06 -0300

    ends game when all players are catched

--- CarameloRun/Gameplay/Components/CatchComponent.swift
+++ CarameloRun/Gameplay/Components/CatchComponent.swift
@@ -9,14 +9,15 @@ import Foundation
 import GameplayKit
 
 class CatchComponent: GKComponent {
-    func didCollideWithPlayer(_ player: Player, _ allRemotePlayers: [Int:Player], finishGame: (() -> Void)?) {
-        print("player \(player.displayName) pegou")
+    func didCollideWithPlayer(_ player: Player, _ allRemotePlayers: [Int:Player], finishGame: (() -> Void)?) async {
+        try? await Task.sleep(nanoseconds: 2_000_000_000) // waits for player state update
         
         // TODO: associar o estado dos outros jogadores | por enquanto a finalizacao do jogo nao funciona quando todos foram presos
-        //if all players are arrested, them ends the game
+        // if all players are arrested, them ends the game
         var allPlayersArrested = true
         for player in allRemotePlayers.values {
-            if !(player.component(ofType: GetCaughtComponent.self)?.isArrested ?? false) {
+            let state = player.component(ofType: PlayerAnimationComponent.self)?.stateMachine.currentState as! CodableState
+            if  PlayerStateStringIdentifier(rawValue: state.stringIdentifier) != .arrestState {
                 allPlayersArrested = false
                 break
             }
