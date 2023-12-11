9fb0810d2c08e5c3266953c639536820cffd1460
Pamella <pamella.as.304@gmail.com>
2023-12-11 17:30:33 -0300

    corrected game center helper

--- CarameloRun/Gameplay/GameCenter/GameCenterHelper.swift
+++ CarameloRun/Gameplay/GameCenter/GameCenterHelper.swift
@@ -17,7 +17,7 @@ final class GameCenterHelper: NSObject, GKLocalPlayerListener {
   weak var delegate: GameCenterHelperDelegate?
 
   private let minPlayers: Int = 2
-  private let maxPlayers: Int = 2
+  private let maxPlayers: Int = 5
 
   private let inviteMessage = "You were invited to play CarameloRun!"
   private var currentVC: GKMatchmakerViewController?
